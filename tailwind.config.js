6e56706eeef33471209bbb3e7a961133de6674b5
mdmmalamin <mdmmalamin@gmail.com>
2022-12-13 12:13:52 +0600

    banner change, 3small card bg color change, header linkedin icon add, footer icon resize and genuine color added

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,13 @@ module.exports = {
 content: ["./*.{html,js}"],
 theme: {
     extend: {},
+    // colors: {
+    //     transparent: 'transparent',
+    //     current: 'currentColor',
+    //     'white': '#ffffff',
+    //     'purple': '#a855f7',
+    //     'indigo': '#4f46e5',
+    // },
 },
 plugins: [require("daisyui")],
 }
